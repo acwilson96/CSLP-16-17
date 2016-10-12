6bdfe0f31f6762ebb2bfa2e4ab8325d02bdb575b
Alex Wilson <Alex@Alexs-MacBook.local>
2016-10-12 15:59:18 +0100

    Updated compile script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 # This script will be used to compile the simulator application 
+javac cslpProgram.java
+javac areaMapMatrix.java
--- simulate.sh
+++ simulate.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-# This script will be used to launch simulations 
+# This script will be used to launch simulations
+input=$(<$1)
+java cslpProgram $input
