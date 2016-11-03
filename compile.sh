605f57a2a04c946142539da033115adecb5695f6
Alex <s1421803@sms.ed.ac.uk>
2016-11-03 15:34:18 +0000

    added compile&simulate.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # This script will be used to compile the simulator application
-javac simStart.java
-javac Simulator.java
 javac areaMapMatrix.java
 javac readfile.java
 javac bin.java
+javac Event.java
+javac simStart.java
+javac Simulator.java
